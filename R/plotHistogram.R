b1da2f6e2c7beaad4ba54a11dbfa7d81a9f90f8d
wStockhausen <william.stockhausen@noaa.gov>
2015-11-07 09:03:59 -0800

    Added plotHistogram() function. Incremented version to 1.1.6.

---
+++ R/plotHistogram.R
@@ -0,0 +1,60 @@
+#'
+#'@title Plot a histogram for a column in a dataframe
+#'
+#'@description Function to plot a histogram for a column in a dataframe
+#'
+#'@param dfr - dataframe
+#'@param x - name of column for histogram
+#'@param label - bin axis label
+#'@param fill - histogram bin colour
+#'@param binwidth - bin width for histogram
+#'@param lines - vector of (nominally vertical) reference line positions
+#'@param colour - colour for reference lines
+#'@param linetype - type for reference lines
+#'@param linesize - size for reference lines
+#'@param reverse - reverse bin order
+#'@param flip - flag to flip orientation
+#'@param facets - expression for faceting using facet_grid()
+#'@param showPlot - flag to print plot
+#'
+#'@return a ggplot2 object
+#'
+#'@import grid
+#'@import ggplot2
+#'
+#'@export
+#'
+plotHistogram<-function(dfr,
+                        x,
+                        label="",
+                        fill='grey50',
+                        binwidth=NULL,
+                        lines=NULL,
+                        colour='black',
+                        linetype=2,
+                        linesize=1.25,
+                        reverse=FALSE,
+                        flip=TRUE,
+                        facets=NULL,
+                        showPlot=FALSE){
+  p <- ggplot(dfr,aes_string(x=x));
+  p <- p + geom_histogram(binwidth=binwidth,fill=fill);
+  if (!is.null(lines)){
+    for (i in 1:length(lines)) p <- p + geom_vline(xintercept=lines[i],colour=colour,linetype=linetype,size=linesize);
+    if (length(lines)>=2){
+      rng<-range(lines,na.rm=TRUE);
+      pct<-0.1*floor(1000*sum((dfr[[x]]<rng[1])|(dfr[[x]]>rng[2]))/length(dfr[[x]]));
+      tg<-grobTree(textGrob(paste(pct,"% outside range",sep=''), x=0.1,  y=0.95, hjust=0,
+  gp=gpar(col="blue", fontsize=15, fontface="italic")));
+      p <- p + annotation_custom(tg);
+      ##p <- p + annotate("text",label=paste('% outside:',pct),x=min(dfr[[x]],na.rm=TRUE),y=1);
+    }
+  }
+  p <- p + xlab(label);
+  if (reverse) p <- p + scale_x_reverse();
+  if (flip)    p <- p + coord_flip();
+  if (!is.null(facets)) p <- p + facet_grid(facets);
+  if (showPlot) print(p);
+  
+  return(p);
+}
