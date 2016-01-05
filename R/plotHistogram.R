86db23a3716daaa033ba63e3662707b263d20d55
wStockhausen <william.stockhausen@noaa.gov>
2016-01-05 13:41:24 -0800

    Revised plotHistogram() to return percent outside range of lines and to be able to turn off display on plot. Incremented version number.

--- R/plotHistogram.R
+++ R/plotHistogram.R
@@ -12,12 +12,15 @@
 #'@param colour - colour for reference lines
 #'@param linetype - type for reference lines
 #'@param linesize - size for reference lines
+#'@param showPct - flag to include \% observations falling outside lines as text on plot
 #'@param reverse - reverse bin order
 #'@param flip - flag to flip orientation
 #'@param facets - expression for faceting using facet_grid()
 #'@param showPlot - flag to print plot
 #'
-#'@return a ggplot2 object
+#'@return a ggplot2 object with an extra list element, 'xtra'. 'xtra' is a 
+#'list with one element: pct.outside, which gives the \% observations falling
+#'outside the range of the 'lines' (if specified), or NULL.
 #'
 #'@import grid
 #'@import ggplot2
@@ -33,21 +36,23 @@ plotHistogram<-function(dfr,
                         colour='black',
                         linetype=2,
                         linesize=1.25,
+                        showPct=FALSE,
                         reverse=FALSE,
                         flip=TRUE,
                         facets=NULL,
                         showPlot=FALSE){
   p <- ggplot(dfr,aes_string(x=x));
   p <- p + geom_histogram(binwidth=binwidth,fill=fill);
+  pct <- NULL;
   if (!is.null(lines)){
     for (i in 1:length(lines)) p <- p + geom_vline(xintercept=lines[i],colour=colour,linetype=linetype,size=linesize);
     if (length(lines)>=2){
       rng<-range(lines,na.rm=TRUE);
       pct<-0.1*floor(1000*sum((dfr[[x]]<rng[1])|(dfr[[x]]>rng[2]))/length(dfr[[x]]));
-      tg<-grobTree(textGrob(paste(pct,"% outside range",sep=''), x=0.1,  y=0.95, hjust=0,
-  gp=gpar(col="blue", fontsize=15, fontface="italic")));
-      p <- p + annotation_custom(tg);
-      ##p <- p + annotate("text",label=paste('% outside:',pct),x=min(dfr[[x]],na.rm=TRUE),y=1);
+      if (showPct){
+        tg<-grobTree(textGrob(paste(pct,"% outside range",sep=''), x=0.1,  y=0.95, hjust=0, gp=gpar(col="blue", fontsize=15, fontface="italic")));
+        p <- p + annotation_custom(tg);
+      }
     }
   }
   p <- p + xlab(label);
@@ -56,5 +61,6 @@ plotHistogram<-function(dfr,
   if (!is.null(facets)) p <- p + facet_grid(facets);
   if (showPlot) print(p);
   
+  p$xtra <- list(pct.outside=pct);
   return(p);
 }
